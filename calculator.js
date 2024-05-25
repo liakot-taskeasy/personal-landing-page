ab216d5874901c23a4d74d2b90c6ffa6d9a6f8ec
liakot-taskeasy <liakot@taskeasy.io>
2024-05-26 02:49:37 +0600

    new task started with flex in css

--- calculator.js
+++ calculator.js
@@ -1,20 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const btn1 = document.getElementById('bt1');
-// console.log(btn1Value);
+// document.addEventListener('DOMContentLoaded', function() {
+//   const btn1 = document.getElementById('bt1');
+// // console.log(btn1Value);
 
+// function handleButtonClick(event) {
+//   // Get the value of the clicked button
+//   const buttonValue = event.target.value;
 
-function handleButtonClick(event) {
-  // Get the value of the clicked button
-  const buttonValue = event.target.value;
+//   // Display the button value in the input
+//   document.getElementById('display').value = buttonValue;
+// }
 
-  // Display the button value in the input
-  document.getElementById('display').value = buttonValue;
-}
+// // Add event listeners to each button
+// btn1.addEventListener('click', handleButtonClick);
 
-// Add event listeners to each button
-btn1.addEventListener('click', handleButtonClick);
+// });
 
-});
-
-
+console.log("a+b");
+var liakot_age = 31 ;
+console.log(liakot_age);
 
